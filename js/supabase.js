11ce44f20c7649ea316441bb514ba65c17c48bbc
Morras-Tics <clubmorrastics@gmail.com>
2026-04-27 02:19:38 +0000

    Actualización

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,6 @@
-import { createClient } from 'https://irftyulcfplwhubkcxpq.supabase.co';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
+// ⚠️ Nota el @2 al final — especifica la versión 2 estable
 
 const supabaseUrl = 'https://irftyulcfplwhubkcxpq.supabase.co';
 const supabaseKey = 'sb_publishable_ujDzFGHQGvzXa6X4nICJcA_etcUOvSv';
